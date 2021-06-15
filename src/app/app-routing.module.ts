a1a6cd45addc09bcc3a3a453729602b0655de1fb
Dersa <dersa85@googlemail.com>
2021-06-15 07:36:54 +0200

    card take and draw

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { GameComponent } from './game/game.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: StartScreenComponent},
+  {path: 'game', component: GameComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
