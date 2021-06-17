02fc7f6e0c40f7825404e85cb8fe36b74f79f7cc
Dersa <dersa85@googlemail.com>
2021-06-17 11:04:34 +0200

    add firebase: { enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDXsiI9MsdX4nE6VGLnnF1bN0IKVmDPWXA",
+    authDomain: "ring-of-fire-249b0.firebaseapp.com",
+    projectId: "ring-of-fire-249b0",
+    storageBucket: "ring-of-fire-249b0.appspot.com",
+    messagingSenderId: "579687311758",
+    appId: "1:579687311758:web:54d4adfdfa60f1ba0c1cdf"
+  }
 };
